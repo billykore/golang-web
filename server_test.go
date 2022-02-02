e9a39c85d332c18db36c65a7dde0296ea4642a70
Billy Kore <billyimmcul2010@gmail.com>
2022-02-02 18:30:06 +0700

    Belajar golang web

---
+++ server_test.go
@@ -0,0 +1,16 @@
+package golang_web
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
